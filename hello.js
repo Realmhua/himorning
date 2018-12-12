0545b18c9658b5d5dfdf31c92145a106c11a6313
huade <guohua74@126.com>
2018-12-12 23:24:01 +0800

    update

---
+++ hello.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let verb = 'hello  ';
+
+function greeting(name) {
+    console.log(verb + name);
+}
+
+module.exports = greeting;
--- spring.js
+++ spring.js
@@ -1,9 +1,23 @@
 'use strict'
 
-console.log(3);
+var greeting = require('./hello');
 
-1+4;
+console.log('hello world');
+greeting('Alfred');
 
-function fun(){
-}
+process.nextTick(() => console.log('nexttick callback'));
+console.log('what\' your name');
 
+let fs = require('fs');
+fs.readFile('README', 'utf-8', (err, data) => {
+    console.log(typeof (err));
+    console.log(data);
+});
+
+fs.stat('README', (err, stat) => {
+    console.log(stat);
+});
+
+process.on('exit', code => {
+    console.log('about to exit with code: ' + code)
+});
